e8c9fd1b9bb5e862345f7b4aa929f7dc31a7aa6e
Tanuka16 <das.tanuka@yahoo.com>
2019-10-23 17:07:51 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import SubtTotal from './components/Subtotal/Subtotal';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      total: 100
+    }
+
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Container className="purchase-card">
+        <h1>ShopNet</h1>
+        <SubtTotal price={this.state.total.toFixed(2)}/>
+      </Container>
     </div>
   );
+}
 }
 
 export default App;
